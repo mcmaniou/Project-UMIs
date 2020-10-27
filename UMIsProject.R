7f0aefb78a9dcfc21eaa182a7915eeb4e1ba6f06
mcmaniou <62932118+mcmaniou@users.noreply.github.com>
2020-10-27 20:42:23 +0200

    Add files via upload

---
+++ UMIsProject.R
@@ -0,0 +1,158 @@
+########## Loading libraries ########## 
+library(tidyverse)
+library(data.table)
+library(ShortRead)
+library(stringr)
+library(Biostrings)
+
+source("functions.R")
+
+########## Inputs ##########
+timeTable <- data.table(part = character(6),
+                        time = character(6))
+
+timeTable$part[1] <- "start time"
+timeTable$time[1] <- format(Sys.time(), "%d-%b-%Y %H.%M.%S")
+
+fastqPath <- list.files(pattern = ".fastq", full = TRUE)
+reads1 <- readFastq(fastqPath[1])
+reads2 <- readFastq(fastqPath[2])
+
+#table with the quality
+# qualityValues <- encoding(quality(reads1))
+
+########## Data preparation ##########
+
+#File 1
+# seq <- as.data.frame(sread(reads1))
+# ids <- as.data.frame(reads1@id)
+
+seq <- as.data.table(sread(reads1))
+ids <- as.data.table(reads1@id)
+
+full <- cbind(seq, ids)
+names(full) <- c("seq", "id")
+
+#the first 12 characters of the string are the UMI and the rest is the read
+full$UMI <- substring(full$seq, 1, 12)
+full$read <- substring(full$seq, 13, 251)
+full <- separate(full, id, c("id1", "id2"), " ", remove = T)
+full <- select(full, read, id1, UMI)
+colnames(full) <- c("read", "id", "UMI")
+
+test_full <- full[,.(count = .N),by=UMI,]
+
+quality <- as(quality(reads1), "matrix")
+# quality <- as.data.frame(quality)
+
+quality = as.data.table(quality)
+
+# shouldn't quality be from 13 to 251 (not UMI's quality included); just like reads
+quality = quality[,13:251]
+
+quality$id <- full$id
+# quality<- as.matrix(quality)
+
+#File 2
+# seq2 <- as.data.frame(sread(reads2))
+# ids2 <- as.data.frame(reads2@id)
+
+seq2 <- as.data.table(sread(reads2))
+ids2 <- as.data.table(reads2@id)
+
+full2 <- cbind(seq2,ids2)
+names(full2) <- c("read", "id")
+full2<- separate(full2,id, c("id1", "id2")," ",remove = T)
+full2 <- select(full2,read, "id1")
+colnames(full2) <- c("read", "id")
+
+quality2 <- as(quality(reads2), "matrix")
+# quality2 <- as.data.frame(quality2)
+
+quality2 = as.data.table(quality2)
+
+quality2$id <- full2$id
+# quality2<- as.matrix(quality2)
+
+rm(ids, ids2, seq, seq2, reads1, reads2)
+
+test_full = test_full[order(test_full$count, decreasing = TRUE), ]
+
+########## First consensus ##########
+timeTable$part[2] <- "beforeFirstConsensus"
+timeTable$time[2] <- format(Sys.time(), "%d-%b-%Y %H.%M.%S")
+
+#test_full <- test_full[1:100,]
+#consensus for each unique UMI
+# result_mean <- lapply(test_full$UMI, groupingFunction, full = full,
+#                       quality = quality, full2 = full2, quality2 = quality2)
+
+result_mean = list()
+
+for(i in test_full$UMI){
+  
+  result_mean[[i]] = groupingFunction(i, full, quality, full2, quality2)
+  
+}
+
+result_mean <- bind_rows(result_mean)
+
+########## UMI correction ##########
+timeTable$part[3] <- "beforeUMIcorrection"
+timeTable$time[3] <- format(Sys.time(), "%d-%b-%Y %H.%M.%S")
+
+newUMIs <- UMIcorrection(test_full,result_mean)
+
+########## Final consensus ##########
+timeTable$part[4] <- "beforeFinalConsensus"
+timeTable$time[4] <- format(Sys.time(), "%d-%b-%Y %H.%M.%S")
+
+consensus_mean = list()
+
+for(i in newUMIs){
+  
+  consensus_mean[[i]] = groupingFinal(i, full, quality, full2, quality2,result_mean)
+  
+}
+
+consensus_mean <- bind_rows(consensus_mean)
+
+#consensus_mean <- lapply(newUMIs$UMI, groupingFinal, full = full,
+#                         quality = quality, full2 = full2, quality2 = quality2, first_consensus = result_mean)
+#consensus_mean <- bind_rows(consensus_mean)
+
+########## Outputs ##########
+timeTable$part[5] <- "beforeOutputs"
+timeTable$time[5] <- format(Sys.time(), "%d-%b-%Y %H.%M.%S")
+
+dir.create("Outputs")
+
+#File1
+file <- ShortReadQ(DNAStringSet(consensus_mean$read1), 
+                   FastqQuality(consensus_mean$quality1),
+                   BStringSet(consensus_mean$UMI))
+
+
+fileSplit1 <- str_split(fastqPath[1],"\\.")
+output <- paste0("Outputs",fileSplit1[[1]][2], "_corrected.fastq.gz")
+file.create(output)
+filepath <- paste0(output)
+writeFastq(file, filepath, mode = "a")
+
+#File2
+file <- ShortReadQ(DNAStringSet(consensus_mean$read2), 
+                   FastqQuality(consensus_mean$quality2),
+                   BStringSet(consensus_mean$UMI))
+
+fileSplit2 <- str_split(fastqPath[2],"\\.")
+output <- paste0("Outputs",fileSplit2[[1]][2], "_corrected.fastq.gz")
+file.create(output)
+filepath <- paste0(output)
+writeFastq(file, filepath, mode = "a")
+
+remove(file, filepath, output, fileSplit1, fileSplit2)
+
+timeTable$part[6] <- "end time"
+timeTable$time[6] <- format(Sys.time(), "%d-%b-%Y %H.%M.%S")
+
+fwrite(timeTable, paste0(getwd(), "/time measurements.csv"),row.names = FALSE, sep = "\t", quote = FALSE)
---
+++ functions.R
@@ -0,0 +1,173 @@
+groupingFunction <-function(r1, full, quality, full2, quality2){
+  
+  #print(r1)
+  #reads with specific UMI
+  # grouping = full[str_detect(full$UMI,as.character(r1)), ]
+  
+  grouping = full[which(full$UMI == r1), ]
+  
+  # grouping = as.data.frame(grouping)
+  
+  grouping2 <- full2[which(full2$id %in% grouping$id), ]
+  
+  quality = quality[which(quality$id %in% grouping$id), ]
+  quality2 = quality2[which(quality2$id %in% grouping2$id), ]
+  
+  grouping = grouping[order(grouping$id), ]
+  grouping2 = grouping2[order(grouping2$id), ]
+  quality = quality[order(quality$id), ]
+  quality2 = quality2[order(quality2$id), ]
+  
+  #File 1
+  # grouping_q <- merge(grouping, quality, by ="id")
+  
+  grouping_q = cbind(grouping, quality[,-c("id")])
+  
+  #File 2
+  grouping2$UMI <- grouping$UMI
+  # grouping_q2 <- merge(grouping2, quality2, by="id")
+  
+  grouping_q2 = cbind(grouping2, quality2[,-c("id")])
+  
+  rm(grouping, grouping2, quality, quality2)
+  
+  result1 <- calculationsFunction(grouping_q) 
+  result2 <- calculationsFunction(grouping_q2)
+  
+  result <- data.table(UMI = substr(r1,1,12),
+                       read1 = result1[1,1], quality1 = result1[1,2], 
+                       read2 = result2[1,1], quality2 = result2[1,2])
+  
+  colnames(result) <- c("UMI" , "read1", "quality1", "read2", "quality2")
+  
+  return(result)
+}
+
+groupingFinal <-function(r1, full, quality, full2, quality2, first_consensus){
+  if (nchar(r1) == 12){
+    
+    result <- first_consensus[which(first_consensus$UMI == r1),]
+    colnames(result) <- c("UMI" , "read1", "quality1", "read2", "quality2")
+    
+  }else{
+    #reads with specific UMI
+    grouping = full[str_detect(full$UMI,as.character(r1)), ]
+    
+    # grouping = as.data.frame(grouping)
+    
+    grouping2 <- full2[which(full2$id %in% grouping$id), ]
+    
+    quality = quality[which(quality$id %in% grouping$id), ]
+    quality2 = quality2[which(quality2$id %in% grouping2$id), ]
+    
+    grouping = grouping[order(grouping$id), ]
+    grouping2 = grouping2[order(grouping2$id), ]
+    quality = quality[order(quality$id), ]
+    quality2 = quality2[order(quality2$id), ]
+    
+    #File 1
+    # grouping_q <- merge(grouping, quality, by ="id")
+    
+    grouping_q = cbind(grouping, quality[,-c("id")])
+    
+    #File 2
+    grouping2$UMI <- grouping$UMI
+    # grouping_q2 <- merge(grouping2, quality2, by="id")
+    
+    grouping_q2 = cbind(grouping2, quality2[,-c("id")])
+    
+    rm(grouping, grouping2, quality, quality2)
+    
+    result1 <- calculationsFunction(grouping_q) 
+    result2 <- calculationsFunction(grouping_q2)
+    
+    result <- data.table(UMI = substr(r1,1,12),
+                         read1 = result1[1,1], quality1 = result1[1,2], 
+                         read2 = result2[1,1], quality2 = result2[1,2])
+    
+    colnames(result) <- c("UMI" , "read1", "quality1", "read2", "quality2")
+  }
+  
+  return(result)
+}
+
+one.run.calculationsFunction <- function(example){
+  
+  # setDT(example)
+  example = example[,.(mean = mean(V2), 
+                       count = .N, 
+                       perc_qual = mean(V2) / 93*100,
+                       perc_count = .N/nrow(example)*100), by = list(V1),]
+  
+  example$criterion = rowMeans(example[,c("perc_qual", "perc_count")])
+  
+  #####!!! what happens if we have the same maximum criterion for different V1 !!!##########
+  
+  return(data.table(V1 = as.character(example[which.max(example$criterion), ]$V1)[1], 
+                    mean = round(example[which.max(example$criterion), ]$mean)[1]))
+  
+}
+
+calculationsFunction <- function(grouping_q){
+  
+  # list with all nts
+  cons = str_split(grouping_q$read, pattern = "", simplify = TRUE)
+  cons = as.list(as.data.table(cons))
+  
+  # list with all qualities
+  grouping_q = as.list(grouping_q[,4:ncol(grouping_q)])
+  
+  # merge lists 
+  grouping_q = base::Map(data.table, cons, grouping_q)
+  
+  rm(cons)
+  
+  ##
+  cons_corr = lapply(grouping_q, one.run.calculationsFunction)
+  cons_corr = rbindlist(cons_corr)
+  
+  #join again in one final sequence
+  consensus = str_c(cons_corr$V1, collapse = "")
+  meanQuality <- as.numeric(cons_corr$mean) + 33 
+  meanQuality <- intToUtf8(meanQuality)
+  result <- data.table( seq = consensus, qual = meanQuality)
+  return(result)
+  
+}
+
+UMIcorrection <- function(test_full,first_consensus){
+  
+  uniqueUMIs <- c()
+
+  while((nrow(test_full) > 0) & (test_full[1,count] > 5)){
+
+    best <- first_consensus[which(first_consensus$UMI == test_full$UMI[1]),]
+    list.best <- best$UMI[1]
+    
+    iterations <- c(2:nrow(test_full))
+    for (i in iterations){
+      
+      base_dist <- stringDist(c(best$UMI[1], test_full$UMI[i]), method = "hamming") 
+      
+      if (base_dist == 1){
+        
+        temp_read1 <- first_consensus[which(first_consensus$UMI ==  test_full$UMI[i]),read1]
+        dist1 <- stringDist(c(best$read1[1], temp_read1), method = "hamming") 
+        
+        temp_read2 <- first_consensus[which(first_consensus$UMI ==  test_full$UMI[i]),read2]
+        dist2 <- stringDist(c(best$read2[1], temp_read2), method = "hamming") 
+        
+        threshold <- 0.05 * (length(temp_read1) + length(temp_read2))
+        if (as.numeric(dist1 + dist2) < threshold){
+          list.best <- paste0(list.best,"|",test_full$UMI[i])
+        }
+      }
+    }
+  
+    uniqueUMIs <- append(uniqueUMIs,list.best)
+    test_full <- test_full[str_detect(test_full$UMI,as.character(list.best), negate = T), ]
+    
+  }
+ 
+  return(uniqueUMIs)
+}
